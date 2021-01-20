c4024139682fe15a1822075b2272baa34eb583bb
Ben Fenster <bfenster@bu.edu>
2021-01-20 11:18:11 -0700

    Relocate SSL certificate to standard location from our custom one.

--- entrypoints.d/apache-ssl.sh
+++ entrypoints.d/apache-ssl.sh
@@ -2,8 +2,5 @@
 
 # Generate self-signed SSL certificate, for use with frontend-to-backend communications:
 set -x
-mkdir -p /etc/ssl/apache
-cd /etc/ssl/apache
-openssl req -x509 -newkey rsa:4096 -keyout cert.key -out cert.pem -days 365 -nodes -subj "/C=US/ST=Massachusetts/L=Boston/O=Boston University/"
+openssl req -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/localhost.key -out /etc/pki/tls/certs/localhost.cert -days 365 -nodes -subj "/C=US/ST=Massachusetts/L=Boston/O=Boston University/"
 set +x
-ls -l /etc/ssl/apache
